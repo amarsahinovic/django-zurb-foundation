0c9c7d013e1ce38281650f4f2b0ac5a792d395e1
Amar Šahinović <amarsahinovic@gmail.com>
2013-06-11 02:03:38 +0200

    Give name to test url

--- foundation/urls.py
+++ foundation/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import patterns
+from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 # Test patern, renders Zurb Foundation default page using base template
 urlpatterns = patterns('',
-    (r'^foundation/', TemplateView.as_view(template_name="foundation/index.html")),
+    url(regex=r'^foundation/', 
+        view=TemplateView.as_view(template_name="foundation/index.html"), 
+        name="foundation_index"),
 )
