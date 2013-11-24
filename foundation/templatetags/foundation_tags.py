b994c4917343574a007da8c159250c7c1022ee30
Amar Šahinović <amarsahinovic@gmail.com>
2013-11-25 00:45:50 +0100

    Update to Foundation 5.0.2

--- foundation/templatetags/foundation_tags.py
+++ foundation/templatetags/foundation_tags.py
@@ -4,6 +4,16 @@ from django.templatetags.static import static
 register = template.Library()
 
 @register.simple_tag
-def foundation_include(js_name):
+def foundation_js(js_name):
     js_path = "foundation/js/foundation/foundation.{0}.js".format(js_name)
     return '<script src="{0}"></script>'.format(static(js_path))
+
+@register.simple_tag
+def foundation_vendor(vendor_name):
+    vendor_path = "foundation/js/vendor/{0}.js".format(vendor_name)
+    return '<script src="{0}"></script>'.format(static(vendor_path))
+
+@register.simple_tag
+def foundation_css(css_name):
+    css_path = "foundation/css/{0}.css".format(css_name)
+    return '<link rel="stylesheet" href="{0}"/>'.format(static(css_path))
