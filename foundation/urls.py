b8f711eb03fbbfb5ea7c9e72ad42b37746e8a8f8
Amar Šahinović <amarsahinovic@gmail.com>
2013-11-25 02:27:02 +0100

    Foundation icons

--- foundation/urls.py
+++ foundation/urls.py
@@ -3,7 +3,11 @@ from django.views.generic import TemplateView
 
 # Test patern, renders Zurb Foundation default page using base template
 urlpatterns = patterns('',
-    url(regex=r'^', 
+    url(regex=r'^$', 
         view=TemplateView.as_view(template_name="foundation/index.html"), 
         name="foundation_index"),
+
+    url(regex=r'^icons/$', 
+        view=TemplateView.as_view(template_name="foundation/icons.html"), 
+        name="foundation_icons"),
 )
