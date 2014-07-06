2a9302df74b45fcb27a5927225b1421dff4d6256
Amar Sahinovic <amarsahinovic@gmail.com>
2014-07-06 19:40:58 +0200

    Add scss files and template for testing it

--- foundation/urls.py
+++ foundation/urls.py
@@ -3,11 +3,15 @@ from django.views.generic import TemplateView
 
 # Test patern, renders Zurb Foundation default page using base template
 urlpatterns = patterns('',
-    url(regex=r'^$', 
-        view=TemplateView.as_view(template_name="foundation/index.html"), 
+    url(regex=r'^$',
+        view=TemplateView.as_view(template_name="foundation/index.html"),
         name="foundation_index"),
 
-    url(regex=r'^icons/$', 
-        view=TemplateView.as_view(template_name="foundation/icons.html"), 
+    url(regex=r'^scss/$',
+        view=TemplateView.as_view(template_name="foundation/scss/index.html"),
+        name="foundation_scss_index"),
+
+    url(regex=r'^icons/$',
+        view=TemplateView.as_view(template_name="foundation/icons.html"),
         name="foundation_icons"),
 )
