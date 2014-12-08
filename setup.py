75645041967eb9f6f38c806db3ce679277845409
Amar Sahinovic <amarsahinovic@gmail.com>
2014-12-08 12:54:14 +0100

    Bump version a little

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ SOURCE_DIR = os.path.join(ROOT_DIR)
 
 setup(
     name="django-zurb-foundation",
-    version="5.4.7",
+    version="5.4.7.1",
     description="Django Zurb Foundation package.",
     author="Amar Šahinović",
     author_email="amar@sahinovic.com",
